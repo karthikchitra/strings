888c73756e4aecbe15a7433806eeff092aeb6a14
Karthik Chitra <karthikchitra272@gmail.com>
2025-10-05 12:59:40 +0530

    Add files via upload

---
+++ Strings.py
@@ -0,0 +1,13 @@
+# Strings is sequence of characters
+# Strings is In-Mutable data types it cant be modify
+# Strings is single quotes and double and triple quotes
+# String is ordered data type its supports indexing values only.
+
+name='chitra karthik'
+print(name[:])# this will be give whole string 
+print(name[0]) # index value of starting c is 0
+print(name[-1]) # index value of last char k is -1
+print(name[0:2]) # it can be index value 0 to 2 values display
+print(name[0:-1]) # it can be print all characters except last index value 
+print(name[::2]) # it can be jump from 2 characters after 2 
+print(name[::-1]) #it can be reversed -1 index value to last to first
